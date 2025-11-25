e354780df32a5c96ee353d55123453e9de7b6aa8
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-11-25 14:01:25 -0800

    product class

---
+++ Product.java
@@ -0,0 +1,17 @@
+public class Product {
+    private String productID;
+    private String name;
+    private float price;
+    private String category;
+    private int quanitity;
+    private String[] tags;
+
+    public Product(String productID, String name, float price, String category, int quantity, String tagList) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.quanitity = quantity;
+        this.tags = tagList.split(";");
+    }
+}
