d0d0cc58e79f13590a9d7246bebb9f1a38077e5b
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-11-29 18:39:26 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        
+        while(true) {
+            System.out.println("Enter choice: \n[1] Quit \n[2] Add new product \n[3] Look up product \n[4] Display products \n[5] Sort list");
+            int choice = scnr.nextInt();
+
+            if(choice == 1) {
+                System.out.println("Existing system");
+                break;
+            }
+            else if(choice == 2) {
+                
+            }
+        }       
     }
 }
